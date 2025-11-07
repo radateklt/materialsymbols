b335d5e822fea868fe1740b93b0a978750525448
Darius <dev@radatek.lt>
2025-11-07 05:19:06 +0100

    Update

--- Icon.vue
+++ Icon.vue
@@ -1,26 +1,36 @@
-<script>
-import { h } from "vue"
+<script lang="ts">
+import { h, defineComponent } from "vue"
+import type { App, Plugin } from "vue"
 import './iconfont.woff2'
 
-export default {
-  props: ["value" ],
-  setup (props, ctx) {
-    return () => h("i", { class: "icon" }, props.value || ctx.slots)
+const Icon = defineComponent({
+  name: "Icon",
+  props: {
+    value: String,
+    filled: {
+      type: Boolean,
+      default: false
+    }
   },
-  install (app) {
+  setup (props, ctx) {
+    return () => h("i", { class: props.filled ? "icon-filled" : "icon" }, props.value || ctx.slots)
+  },
+  install (app: App) {
     app.component("Icon", this)
   }
-}
+})
+
+export default Icon as typeof Icon & Plugin
 </script>
 
 <style>
 @font-face {
   font-family: "Material Symbols";
   font-style: normal;
-  font-weight: 400;
+  font-weight: 700;
   src: url(iconfont.woff2) format("woff2");
 }
-.icon {
+.icon, .icon-filled {
   font-family: "Material Symbols";
   font-weight: normal;
   font-style: normal;
@@ -38,5 +48,8 @@ export default {
   font-feature-settings: "liga";
   vertical-align: middle;
   font-display: block;
+}
+.icon-filled {
+  font-variation-settings: 'FILL' 1;
 }
 </style>
